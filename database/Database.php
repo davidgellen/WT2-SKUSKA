cfa6bbd09cf787a456a1662ba1bbb568423230d4
Zuzana Medzihradska <zuzkame@gmail.com>
2021-04-18 12:40:47 +0200

    db

--- database/Database.php
+++ database/Database.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once "config.php";
 
 class Database{
 
     private $conn;
 
     public function getConnection(){
+        require_once "config.php";
         $this->conn = null;
         try{
-            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+            $this->conn = new PDO("mysql:host=" . $servername . ";dbname=" . $dbname, $username, $password);
             $this->conn->exec("set names utf8");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch (PDOException $exception){
