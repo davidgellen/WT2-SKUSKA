eb8e0bed8c96c8136d50a22b42e75161cf9bc0fc
Zuzana Medzihradska <zuzkame@gmail.com>
2021-04-18 15:32:47 +0200

    style added

--- teacherProfil.php
+++ teacherProfil.php
@@ -1,5 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <?php include "includes/header.php" ?>
+    <link rel="stylesheet" href="styles/styleBody.css">
+</head>
+<body>
+    
 
-//TODO: Pridavanie testov, etc.
-
-?>
+<?php include "includes/footer.php";?>
+</body>
+</html>
