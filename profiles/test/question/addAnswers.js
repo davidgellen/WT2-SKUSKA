86185eb431b5091486a5febe88e457b6eec6b853
Zuzana Medzihradska <zuzkame@gmail.com>
2021-05-09 22:00:38 +0200

    add multi-answer question

---
+++ profiles/test/question/addAnswers.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+    $("#addCorrect").click(function (e) {
+        $("#correctAnswers").append("<br><textarea class='correct' cols='60' rows='2'></textarea><br>");
+    });
+    
+    $("#addWrong").click(function (e) {
+        $("#wrongAnswers").append("<br><textarea class='wrong' cols='60' rows='2'></textarea><br>");
+    });
+
+    
+    $("#submitForm").click(function (e) {
+        var correctData = Array();
+        var wrongData = Array();
+        var points = $("#points").val();
+        var questionText = $("#questionText").val();
+
+        for(var i = 0; i<$(".correct").length; i++){
+            correctData.push($(".correct")[i].value);
+        }
+        for(var i = 0; i<$(".wrong").length; i++){
+            wrongData.push($(".wrong")[i].value);
+        }
+
+        if(correctData==null) correctData = {};
+        if(wrongData==null) wrongData = {};
+
+        var data = {
+            questionText: questionText,
+            correct: correctData,
+            wrong: wrongData,
+            points: points,
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '../../../scripts/questions/createMultipleAnswer.php',
+            data: data,
+            datatype: 'json',
+            encode: true,
+            success: function(data){
+                console.log(data);
+                var redirect = "../detail.php?test=" + data;
+                window.location.href = redirect;
+            }
+        })
+    });
+    
+    })
